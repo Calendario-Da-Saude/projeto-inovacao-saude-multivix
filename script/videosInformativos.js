d25778fe20c634f2a4c759349cd41cc0d3d19148
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-05-30 15:13:43 -0300

    Update

--- script/videosInformativos.js
+++ script/videosInformativos.js
@@ -1,44 +1,61 @@
 const containerVideo = document.getElementsByClassName("containerVideo");
 const containerTitulo = document.getElementsByClassName("containerTitulo");
+const videos = document.querySelectorAll("video");
 
-for (let elemento of containerVideo) {
-  elemento.addEventListener("click", function() {
-    for (let elemento of containerVideo) {
+for (let elemento of containerTitulo) {
+  elemento.addEventListener("click", function () {
+    for (let elemento of containerTitulo) {
       if (elemento !== this) {
-        let video = elemento.children[1];
+        let containerVideoPai = this;
+        let containerVideoFilho = containerVideoPai.parentNode;
+        let video = containerVideoFilho.children[1];
+
+        for (let elemento of videos) {
+          elemento.pause();
+        }
         video.style.display = "none";
         elemento.classList.remove("clicou");
       }
     }
 
-    let video = this.children[1];
+    let containerVideoPai = this;
+    let containerVideoFilho = containerVideoPai.parentNode;
+    let video = containerVideoFilho.children[1];
+
     this.classList.toggle("clicou");
 
     if (this.classList.contains("clicou")) {
       video.style.display = "block";
     } else {
+      for (let elemento of videos) {
+        elemento.pause();
+      }
       video.style.display = "none";
     }
   });
 }
 
 for (let elemento of containerTitulo) {
-  elemento.addEventListener("click", function() {
-
+  elemento.addEventListener("click", function () {
     for (let elemento of containerTitulo) {
-        if (elemento !== this) {
-            let thisArrow = elemento.children[1];
-            thisArrow.classList.remove("rotate-90-cw")
-            thisArrow.classList.add("rotate-back");
-        }
+      if (elemento !== this) {
+        let thisArrow = elemento.children[1];
+        let thisArrowPai = thisArrow.parentNode;
+        let containerVideo = thisArrowPai.parentNode;
+        let video = containerVideo.children[1];
+        thisArrow.classList.remove("rotate-90-cw");
+        thisArrow.classList.add("rotate-back");
+        video.style.display = "none";
       }
+    }
 
     let thisArrow = this.children[1];
     if (thisArrow.classList.contains("rotate-90-cw")) {
-        thisArrow.classList.add("rotate-back")
+      thisArrow.classList.remove("rotate-90-cw");
+      thisArrow.classList.add("rotate-back");
     } else {
-        thisArrow.classList.remove("rotate-back")
+      thisArrow.classList.remove("rotate-back");
+      thisArrow.classList.add("rotate-90-cw");
     }
-    thisArrow.classList.toggle("rotate-90-cw");
   });
 }
