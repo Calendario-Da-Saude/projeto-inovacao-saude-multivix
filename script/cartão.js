c09cf66c6572ad2403b8133af9e388443e96edb1
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-06-01 12:30:58 -0300

    Update

--- script/cartão.js
+++ script/cartão.js
@@ -1,5 +1,10 @@
 const avatar = document.getElementsByClassName("avatar");
 const containerPerfis = document.getElementById("containerPerfis");
+const criançaVacina = document.getElementById("criançaVacina");
+const adolescenteVacina = document.getElementById("adolescenteVacina");
+const adultoVacina = document.getElementById("adultoVacina");
+const gestanteVacina = document.getElementById("gestanteVacina");
+const idosoVacina = document.getElementById("idosoVacina");
 
 for (let elemento of avatar) {
     elemento.addEventListener("click", function() {
@@ -10,13 +15,29 @@ for (let elemento of avatar) {
         for (let i = 0; i < avatar.length; i++) {
             let elemento = avatar[i];
             if (i === 2 && elemento.classList.contains("selecionado")) {
+                criançaVacina.style.display = "none";
+                adolescenteVacina.style.display = "none";
+                adultoVacina.style.display = "block";
                 containerPerfis.style.transform = "translateX(-150px)"
             } else if (i === 1 && elemento.classList.contains("selecionado")) {
+                criançaVacina.style.display = "none";
+                adolescenteVacina.style.display = "block";
                 containerPerfis.style.transform = "translateX(-30px)"
             } else if (i === 3 && elemento.classList.contains("selecionado")) {
+                criançaVacina.style.display = "none";
+                adolescenteVacina.style.display = "none";
+                adultoVacina.style.display = "none";
+                gestanteVacina.style.display = "block";
                 containerPerfis.style.transform = "translateX(-310px)"
             } else if (i === 0 && elemento.classList.contains("selecionado")) {
+                criançaVacina.style.display = "block"
                 containerPerfis.style.transform = "translateX(0)"
+            } else if (i === 4 && elemento.classList.contains("selecionado")) {
+                criançaVacina.style.display = "none";
+                adolescenteVacina.style.display = "none";
+                adultoVacina.style.display = "none";
+                gestanteVacina.style.display = "none";
+                idosoVacina.style.display = "block";
             }
         }
     })
