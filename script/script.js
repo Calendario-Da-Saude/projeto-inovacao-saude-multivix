1465deced2e73b5a615ddad406bef6f539e791c3
Guilherme Oliveira <128637003+Calendario-Da-Saude@users.noreply.github.com>
2023-06-14 17:08:15 -0300

    update

--- script/script.js
+++ script/script.js
@@ -12,40 +12,40 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 })
 
-btnCadastro.addEventListener("click", function () {
-  janelaCadastro.style.display = "block";
-});
+// btnCadastro.addEventListener("click", function () {
+//   janelaCadastro.style.display = "block";
+// });
 
-fecharCadastro.addEventListener("click", function() {
-    janelaCadastro.style.display = "none";
-});
+// fecharCadastro.addEventListener("click", function() {
+//     janelaCadastro.style.display = "none";
+// });
 
-btnCadastrar.addEventListener("click", function () {
+// btnCadastrar.addEventListener("click", function () {
    
-});
+// });
 
 
-document.addEventListener("click", function (event) {
-    if (!btnCadastro.contains(event.target) && !janelaCadastro.contains(event.target)) {
-        janelaCadastro.style.display = "none";
-    }
-});
+// document.addEventListener("click", function (event) {
+//     if (!btnCadastro.contains(event.target) && !janelaCadastro.contains(event.target)) {
+//         janelaCadastro.style.display = "none";
+//     }
+// });
 
-btnLogin.addEventListener("click", function () {
-  janelaLogin.style.display = "block";
-});
+// btnLogin.addEventListener("click", function () {
+//   janelaLogin.style.display = "block";
+// });
 
-btnLogin.addEventListener("click", function() {
+// btnLogin.addEventListener("click", function() {
     
-});
+// });
 
-btnLogin.addEventListener("click", function () {
+// btnLogin.addEventListener("click", function () {
    
-});
+// });
 
 
-document.addEventListener("click", function (event) {
-    if (!btnLogin.contains(event.target) && !janelaLogin.contains(event.target)) {
-        janelaLogin.style.display = "none";
-    }
-});
+// document.addEventListener("click", function (event) {
+//     if (!btnLogin.contains(event.target) && !janelaLogin.contains(event.target)) {
+//         janelaLogin.style.display = "none";
+//     }
+// });
